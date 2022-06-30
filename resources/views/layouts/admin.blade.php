12733539b25fee7efa72eeeff03c5a151d9c014a
farjana01 <nipa.farzana87@gmail.com>
2022-07-01 00:53:03 +0600

    admin layout completed

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+            <div class="">
+                <div class="container-fluid">
+                    <div class="row flex-nowrap">
+                        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                                    <span class="fs-5 d-none d-sm-inline">Menu</span>
+                                </a>
+                                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                                    <li class="nav-item">
+                                        <a href="{{ route('admin.roles.index') }}" class="nav-link align-middle px-0">
+                                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Roles</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ route('admin.permissions.index') }}" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Permissions</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
+                                            <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+                                        <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                                            <li class="w-100">
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
+                                            <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                                            <li class="w-100">
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+        
+                                </ul>
+                                <hr>
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <div class="d-inline-block">{{ Auth::user()->name }}</div>
+                                    </button>
+                                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                                      <li>
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                
+                                            <x-dropdown-link class="dropdown-item text-white" :href="route('logout')"
+                                                    onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                                {{ __('Log Out') }}
+                                            </x-dropdown-link>
+                                        </form>
+                                      </li>
+                                      {{-- <li><a class="dropdown-item" href="#"></a></li> --}}
+                                    </ul>
+                                  </div>
+                            </div>
+                        </div>
+                        <div class="col py-3">
+                            <main>
+                                {{ $slot }}
+                            </main>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Page Content -->
+    
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
+use GuzzleHttp\Psr7\Uri;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +24,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->group(function(){
+    Route::get(uri:'/', action:[IndexController::class, 'index'])->name('index');
+    Route::resource('/roles', RoleController::class);
+    Route::resource('/permissions', PermissionController::class);
+});
 
 require __DIR__.'/auth.php';
